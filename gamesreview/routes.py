bd22b749f69a156d0a55abaf6f4a99e5c668d0e8
Jon9851 <joncohen21k@hotmail.co.uk>
2022-05-04 15:01:45 +0000

    flask postgres sql alchemy monof and Werkzeug insatlled. files for templates and games review folders created with app routes run env and init.py all created

---
+++ gamesreview/routes.py
@@ -0,0 +1,5 @@
+from flask import flash, render_template, request, redirect, session, url_for
+from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
+from taskmanager import app, db, mongo
+from taskmanager.models import Category, Users
