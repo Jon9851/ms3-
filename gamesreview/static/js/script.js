4daad4197503d804071bfdd9403253ad3143b48d
Jon9851 <joncohen21k@hotmail.co.uk>
2022-05-06 11:03:59 +0000

    image sidler added, with custome javascript and image transition

---
+++ gamesreview/static/js/script.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // sidenav initialization
+    let sidenav = document.querySelectorAll(".sidenav");
+    M.Sidenav.init(sidenav);
+});
+
+//  image slider
+let counter = 1
+
+
+
+setInterval(()=>{
+
+    document.querySelector('.img.show').classList.remove('show')
+    const img = document.querySelector(`.img-${counter}`)
+    img.classList.add('show')
+    counter++
+
+    if(counter >4){
+        counter = 1
+    }
+
+},3000)
