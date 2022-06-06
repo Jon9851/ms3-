d692d5fae690d6748c338c15923b6c7fde933ba6
Jon9851 <joncohen21k@hotmail.co.uk>
2022-06-06 14:48:30 +0000

    login functions added with profile pages added too, logout function just needs adding

--- gamesreviewbeta/routes.py
+++ gamesreviewbeta/routes.py
@@ -23,9 +23,41 @@ mongoapp.config["MONGO_URI"] = os.getenv('MONGO_URI')
 def index():
     return render_template("index.html")
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
+    if request.method == "POST":
+        # check if username exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            # ensure hashed password matches user input
+            if check_password_hash(
+                existing_user["password"], request.form.get("password")):
+                    session["user"] = request.form.get("username").lower()
+                    flash("Welcome, {}".format(
+                        request.form.get("username")))
+                    return redirect(url_for(
+                        "profile", username=session["user"]))
+            else:
+                # invalid password match
+                flash("Incorrect Username or Password")
+                return redirect(url_for("login"))
+
+        else:
+            # username doesn't exist
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("login"))
+
     return render_template("login.html")
+# User name routes
+@app.route("/profile/<username>", methods=["GET", "POST"])
+def profile(username):
+    # grab the session user's username from db
+    username = mongo.db.users.find_one(
+        {"username": session["user"]})["username"]
+    return render_template("profile.html", username=username)
+
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
@@ -48,6 +80,9 @@ def register():
         # put the new user into 'session' cookie
         session["user"] = request.form.get("username").lower()
         flash("Registration Succesfull")
+        return redirect(url_for("profile", username=session["user"]))
+
+
     return render_template("register.html")
 
 @app.route("/games")
