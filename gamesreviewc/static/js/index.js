df98dbc00240a8b841d452314c1a49627f892207
Jon9851 <joncohen21k@hotmail.co.uk>
2022-07-18 13:52:14 +0000

    fixed the javascript error

--- gamesreviewc/static/js/index.js
+++ gamesreviewc/static/js/index.js
@@ -34,3 +34,35 @@ window.onload = function () {
     plusSlides(1);
   }, 3000);
 };
+
+//  image Slider
+var slides = document.querySelectorAll('.slide');
+var buttons = document.querySelectorAll('.btn');
+let currentSlide = 1;
+
+//Autoplay Image Slider 
+var repeat = function(activeClass){
+    let active = document.getElementsByClassName('active');
+    let i = 1;
+
+    var repeater = () => {
+        setTimeout(function(){
+            [...active].forEach((activeSlide) => {
+            activeSlide.classList.remove('active');
+    });
+
+    slides[i].classList.add('active');
+    i++;
+
+    if(slides.length == i){
+        i = 0;
+    }
+    if(i >= slides.length){
+        return;
+    }
+    repeater();
+        }, 5000);
+    }
+    repeater();
+}
+repeat();
--- gamesreviewc/static/js/script.js
+++ gamesreviewc/static/js/script.js
@@ -16,34 +16,3 @@
     M.Modal.init(modal);
 });
 
-//  image Slider
-var slides = document.querySelectorAll('.slide');
-var buttons = document.querySelectorAll('.btn');
-let currentSlide = 1;
-
-//Autoplay Image Slider 
-var repeat = function(activeClass){
-    let active = document.getElementsByClassName('active');
-    let i = 1;
-
-    var repeater = () => {
-        setTimeout(function(){
-            [...active].forEach((activeSlide) => {
-            activeSlide.classList.remove('active');
-    });
-
-    slides[i].classList.add('active');
-    i++;
-
-    if(slides.length == i){
-        i = 0;
-    }
-    if(i >= slides.length){
-        return;
-    }
-    repeater();
-        }, 5000);
-    }
-    repeater();
-}
-repeat();
